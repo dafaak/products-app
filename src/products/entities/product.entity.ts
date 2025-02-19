f5e372e8d9028c72b5d54e5b2151a85e324a5186
Israel <israel.pe@outlook.es>
2025-02-18 21:17:27 -0500

    fat: update product

--- src/products/entities/product.entity.ts
+++ src/products/entities/product.entity.ts
@@ -1,3 +1,9 @@
+interface UpdateWithOptions {
+  name?: string;
+  price?: number;
+  description?: string;
+}
+
 export class Product {
   constructor(
     public id: string,
@@ -9,5 +15,11 @@ export class Product {
     this.name = name;
     this.price = price;
     if (description) this.description = description;
+  }
+
+  updateWith({ name, price, description }: UpdateWithOptions) {
+    this.name = name ?? this.name;
+    this.price = price ?? this.price;
+    this.description = description ?? this.description;
   }
 }
--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -7,36 +7,36 @@ import { Product } from './entities/product.entity';
 @Injectable()
 export class ProductsService {
   private products: Product[] = [
-    {
-      id: 'f89e9379-71c6-4994-8813-7c63e99b0ecf',
-      name: 'iPhone 13',
-      price: 799,
-      description: 'Latest model with A15 Bionic chip',
-    },
-    {
-      id: '1a9d0a9f-8c43-4faf-8acb-c53e6a02240c',
-      name: 'MacBook Pro',
-      price: 1299,
-      description: '16-inch, M1 Pro chip',
-    },
-    {
-      id: 'aa60a4a2-dd62-46b0-8f04-00ca740e88d8',
-      name: 'iPad Pro',
-      price: 999,
-      description: '12.9-inch, M1 chip',
-    },
-    {
-      id: 'b5d4179c-d7b1-485c-ba06-4e18e01aa0fe',
-      name: 'Apple Watch Series 7',
-      price: 399,
-      description: '45mm, GPS + Cellular',
-    },
-    {
-      id: '32c64e71-1108-4eb4-8154-645b5dd82c5a',
-      name: 'AirPods Pro',
-      price: 249,
-      description: 'Active Noise Cancellation',
-    },
+    new Product(
+      'f89e9379-71c6-4994-8813-7c63e99b0ecf',
+      'iPhone 13',
+      799,
+      'Latest model with A15 Bionic chip',
+    ),
+    new Product(
+      '1a9d0a9f-8c43-4faf-8acb-c53e6a02240c',
+      'MacBook Pro',
+      1299,
+      '16-inch, M1 Pro chip',
+    ),
+    new Product(
+      'aa60a4a2-dd62-46b0-8f04-00ca740e88d8',
+      'iPad Pro',
+      999,
+      '12.9-inch, M1 chip',
+    ),
+    new Product(
+      'b5d4179c-d7b1-485c-ba06-4e18e01aa0fe',
+      'Apple Watch Series 7',
+      399,
+      '45mm, GPS + Cellular',
+    ),
+    new Product(
+      '32c64e71-1108-4eb4-8154-645b5dd82c5a',
+      'AirPods Pro',
+      249,
+      'Active Noise Cancellation',
+    ),
   ];
 
   create(createProductDto: CreateProductDto) {
@@ -58,7 +58,13 @@ export class ProductsService {
   }
 
   update(id: string, updateProductDto: UpdateProductDto) {
-    return `This action updates a #${id} product`;
+    const { id: __, name, description, price } = updateProductDto;
+
+    const product = this.findOne(id);
+
+    product.updateWith({ name, price, description });
+
+    return product;
   }
 
   remove(id: string): Product {
