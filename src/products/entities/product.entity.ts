60dc927ce936589c7cdeeee80953c31e1f137018
Israel <israel.pe@outlook.es>
2025-02-18 20:44:46 -0500

    feat: entity products

--- src/products/entities/product.entity.ts
+++ src/products/entities/product.entity.ts
@@ -1,1 +1,13 @@
-export class Product {}
+export class Product {
+  constructor(
+    public id: string,
+    public name: string,
+    public price: number,
+    public description?: string,
+  ) {
+    this.id = id;
+    this.name = name;
+    this.price = price;
+    if (description) this.description = description;
+  }
+}
